c7be882ea534461728c518d16b19eb709a49fc6f
atefhejazi1 <atefhejazi10@gmail.com>
2021-04-28 18:28:39 +0300

    first commit

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    style: {
+        postcss: {
+            plugins: [
+                // require('postcss-import'),
+                require('tailwindcss'),
+                // require('postcss-nested'),
+                require('autoprefixer'),
+            ],
+        },
+    },
+}
